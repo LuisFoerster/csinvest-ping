bcfbd5dfdd27db50eac1db01006d631fdd8ec871
Luis Förster <luis.foerster@t-online.de>
2023-03-17 11:26:33 +0100

    fixed bug #2

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -7,7 +7,7 @@ def ping():
 
 @app.route('/hello/<name>')
 def hello(name):
-    return 'Hello, ' + str(name)
+    return 'Hello, ' + str(name) #Hier wurde bug gefixt
 
 @app.route('/json', methods=['GET'])
 def json():
